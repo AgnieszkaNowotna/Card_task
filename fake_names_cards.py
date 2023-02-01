c6a4dd2d34db54c0a0fff72495b7ec519f34b2b4
Agnieszka Nowotna <a.ilkow7@gmail.com>
2023-02-01 12:10:40 +0100

    Delete class Card

--- fake_names_cards.py
+++ fake_names_cards.py
@@ -2,31 +2,7 @@ from faker import Faker
 
 fake = Faker()
 
-class Card:
-    def __init__(self,name,surname,company,job,email):
-        self.name = name
-        self.surname = surname
-        self.company = company
-        self.job = job
-        self.email = email
-        self._number_of_signs = 0
-
-    def __repr__(self):
-        return (f"""Showcase(name = {self.name} surname = {self.surname},company = {self.company} job = {self.job} email = {self.email}""")
-
-    def __str__(self):
-        return f'{self.name} {self.surname},{self.email}'
-
-    def contact (self):
-        print(f'Kontaktuję się z {self.name} {self.surname}, {self.job}, {self.email}')
-
-    @property
-    def label_lenght(self):
-        all_signs = len(self.name) + len(self.surname) + 1
-        self._number_of_signs = all_signs
-        return self._number_of_signs
-
-class BaseContact(Card):
+class BaseContact():
     def __init__(self, name, surname, telephone_number, email):
         self.name = name
         self.surname = surname
@@ -38,6 +14,12 @@ class BaseContact(Card):
 
     def contact(self):
         print(f'Wybieram numer {self.telephone_number} i dzwonię do {self.name} {self.surname}')
+
+    @property
+    def label_lenght(self):
+        all_signs = len(self.name) + len(self.surname) + 1
+        self._number_of_signs = all_signs
+        return self._number_of_signs
 
 class BusinessContact(BaseContact):
     def __init__(self, business_number,job, company, *args, **kwargs):
@@ -51,25 +33,6 @@ class BusinessContact(BaseContact):
 
     def contact(self):
         print(f'Wybieram numer {self.business_number} i dzwonię do {self.name} {self.surname}')
-
-list_of_showcases = []
-
-for i in range (5):
-    name_surname = (fake.name()).split(" ")
-    name1 = name_surname[0]
-    surname1 = name_surname [-1]
-    person = Card(name = name1,
-                      surname = surname1,
-                      company = fake.company(), 
-                      job = fake.job(),
-                      email = fake.email())
-    print(person)
-    person.contact()
-    list_of_showcases.append(person)
-
-by_name = sorted(list_of_showcases, key = lambda person: person.name)
-by_surname = sorted(list_of_showcases, key = lambda person: person.surname)
-by_email = sorted(list_of_showcases, key = lambda person: person.email)
 
 def create_contact(type, amount):
     for i in range (amount):
@@ -98,4 +61,4 @@ def create_contact(type, amount):
         else:
             print("Wprowadzono błędny rodzaj wizytówki")
 
-create_contact("business",6)
+create_contact("base",10)
